e898a17f98baa58ad6a43441c7da411006632ea0
Valentin Yverneau <valentin.yverneau@epita.fr>
2018-12-14 19:48:25 +0100

    New strat to counter certain strats

--- src/Game/PlayerIA/YadavacPlayer.php
+++ src/Game/PlayerIA/YadavacPlayer.php
@@ -18,36 +18,41 @@ class YadavacPlayer extends Player
 
     public function getChoice()
     {
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get all the Choices          ?    $this->result->getChoicesFor($this->mySide)
-        // How to get the opponent Last Choice ?    $this->result->getChoicesFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the stats                ?    $this->result->getStats()
-        // How to get the stats for me         ?    $this->result->getStatsFor($this->mySide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // How to get the stats for the oppo   ?    $this->result->getStatsFor($this->opponentSide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the number of round      ?    $this->result->getNbRound()
-        // -------------------------------------    -----------------------------------------------------
-        // How can i display the result of each round ? $this->prettyDisplay()
-        // -------------------------------------    -----------------------------------------------------
+        $nbRound = $this->result->getNbRound();
+        $foeChoice = parent::foeChoice();
+        $friendChoice = parent::friendChoice();
 
-        if ($this->result->getNbRound() == 0) {
-            return parent::foeChoice();
+        if ($nbRound == 0) {
+            return $foeChoice;
         }
-        if ($this->result->getLastChoiceFor($this->opponentSide) == parent::foeChoice())
-            return parent::friendChoice();
-        return parent::foeChoice();
+        else if ($nbRound < 3) {
+            if ($this->result->getLastChoiceFor($this->opponentSide) == $foeChoice)
+                return $friendChoice;
+            return $foeChoice;
+        }
+
+        //Opponent plays always foe
+        if (($this->result->getChoicesFor($this->opponentSide)[$nbRound - 3] == $foeChoice)
+            && ($this->result->getChoicesFor($this->opponentSide)[$nbRound - 2] == $foeChoice)
+            && ($this->result->getChoicesFor($this->opponentSide)[$nbRound - 1] == $foeChoice))
+            return $foeChoice;
+
+        //Opponent plays always friend
+        if (($this->result->getChoicesFor($this->opponentSide)[$nbRound - 3] == $friendChoice)
+            && ($this->result->getChoicesFor($this->opponentSide)[$nbRound - 2] == $friendChoice)
+            && ($this->result->getChoicesFor($this->opponentSide)[$nbRound - 1] == $friendChoice))
+            return $foeChoice;
+
+        //Opponent plays the same as my previous choice
+        if (($this->result->getChoicesFor($this->opponentSide)[$nbRound - 2] == $this->result->getChoicesFor($this->mySide)[$nbRound - 3])
+            && ($this->result->getChoicesFor($this->opponentSide)[$nbRound - 1] == $this->result->getChoicesFor($this->mySide)[$nbRound - 2]))
+            return $this->result->getChoicesFor($this->opponentSide)[$nbRound - 1];
+
+        //Opponent plays the reverse of my previous choice
+        if (($this->result->getChoicesFor($this->opponentSide)[$nbRound - 2] != $this->result->getChoicesFor($this->mySide)[$nbRound - 3])
+            && ($this->result->getChoicesFor($this->opponentSide)[$nbRound - 1] != $this->result->getChoicesFor($this->mySide)[$nbRound - 2]))
+            return $foeChoice;
+
+        return $friendChoice;
     }
- 
 };
